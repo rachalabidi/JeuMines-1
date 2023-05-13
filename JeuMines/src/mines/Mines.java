a2c9e478b327cf2d0bda8d7b8c8e86126cba6b00
rachalabidi <rashalabidi@gmail.com>
2023-05-13 18:23:08 +0200

    Making  final fields static too on the Mines Class

--- JeuMines/src/mines/Mines.java
+++ JeuMines/src/mines/Mines.java
@@ -10,8 +10,8 @@ import javax.swing.JLabel;
 public class Mines extends JFrame {
 	private static final long serialVersionUID = 4772165125287256837L;
 	
-	private final int WIDTH = 250;
-    private final int HEIGHT = 290;
+	private  static  final int WIDTH = 250;
+    private static final int HEIGHT = 290;
 
     private JLabel statusbar;
     
